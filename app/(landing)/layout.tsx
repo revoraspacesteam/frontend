62e41ca5a7a9cb3aa677f66dfde482d639f122fc
revoraspacesteam <revoraspacesteam@gmail.com>
2026-08-19 18:08:55 +0530

    Add booking form API integration

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -10,6 +10,7 @@ export default function LandingLayout({
   return (
     <LanguageProvider>
       <Header />
+      
       <main className="flex-1">{children}</main>
       <Footer />
     </LanguageProvider>
